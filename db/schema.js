e4ad7b04be58113868abb3c50323780c7d313d68
jack-schultz <contact@jackschultz.dev>
2026-06-27 12:46:07 +1000

    Added ability to create, view and edit bookings with local SQLite

--- db/schema.js
+++ db/schema.js
@@ -6,12 +6,20 @@ export const AppSchema = new Schema({
             id: column.text,
             created_at: column.text,
             restaurant_id: column.integer,
+            profile_id: column.text,
+
+            datetime: column.text,
             first_name: column.text,
             last_name: column.text,
+            phone_number: column.text,
+            email: column.text,
+            total_pax: column.integer,
+            adult_pax: column.integer,
+            child_pax: column.integer,
+            hc_pax: column.integer,
             preference: column.text,
-            datetime: column.text,
-            profile_id: column.text,
             status: column.text,
+            notes: column.text,
         },
         {
             indexes: {
@@ -21,7 +29,6 @@ export const AppSchema = new Schema({
         }
     ),
 
-    // Local-only table for tracking SQL migrations (not synced to Supabase).
     migrations: new Table(
         {
             name: column.text,
